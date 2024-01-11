fe7465f619f40ed0b7da8626e236ad1235bc704e
Divyanshu Mac <pandagre007@gmail.com>
2024-01-11 23:41:50 +0530

    test form in Testing

--- src/components/Input/CustomInput.js
+++ src/components/Input/CustomInput.js
@@ -3,10 +3,10 @@ import { View, Text, TextInput, StyleSheet, TouchableOpacity, Animated } from "r
 import { screenWidth } from "../../appStyles/colorStyle";
 import { Validations } from "./Validations";
 
-export const Form =()=>{
+export const CustomInput =({ label })=>{
+
     const [input, setInput] = useState('');
     const [startEdit, setStartEdit] = useState(false);
-    // const [opacity, setOpacity] = useState(0);
 
     //Position 
     const position = useRef(new Animated.Value(0)).current;
@@ -24,15 +24,15 @@ export const Form =()=>{
     const fadeIn = () => {
         Animated.timing(position, {
           toValue: -20,
-          duration: 400,
+          duration: 350,
           useNativeDriver: true,
         }).start();
 
         Animated.timing(opacity, {
             toValue: 1,
-            duration: 400,
+            duration: 350,
             useNativeDriver: true,
-          }).start();
+        }).start();
       };
 
     //Fade Out
@@ -47,45 +47,32 @@ export const Form =()=>{
             toValue: 0,
             duration: 300,
             useNativeDriver: true,
-          }).start();
+        }).start();
     };
 
     return(
         <View style={styles.container}>
             <View>
                 {/* {!startEdit? null : */}
-                    <Animated.Text style={[styles.label, {transform:[{translateY: position}], opacity:opacity}]}>Name</Animated.Text>
+                    <Animated.Text style={[styles.label, {transform:[{translateY: position}], opacity:opacity}]}>{label ? label : 'Specify Label'}</Animated.Text>
                 {/* } */}
                 <TextInput
                     style={styles.input}
                     onChangeText={(value)=> setInput(value)}
-                    placeholder={startEdit? '':"Name"}
+                    placeholder={startEdit? '':label ? label : 'Specify Label'}
                     onFocus={
                         ()=>{
                             fadeIn();
-                            //setStartEdit(true)
+                            setStartEdit(true)
                         }
                     }
                     onEndEditing={()=>{
                             fadeOut();
-                            //setStartEdit(false)
+                            setStartEdit(false)
                         }
                     }
                 />
             </View>
-            <TextInput
-                    style={[styles.input, {marginTop:10}]}
-                    onChangeText={(value)=> setInput(value)}
-                    placeholder="Name"
-                    //onFocus={()=>setStartEdit(true)}
-                    //onEndEditing={()=>setStartEdit(false)}
-                />
-            <TouchableOpacity 
-                style={styles.submitButton}
-                onPress={handleInput}
-            >
-                <Text style={styles.buttonText}>Submit</Text>
-            </TouchableOpacity>
         </View>
     );
 }
@@ -108,7 +95,6 @@ const styles = StyleSheet.create({
         backgroundColor:"#ffffff"
     },
     input:{
-
         backgroundColor:"#ffffff",
         width:screenWidth-50,
         height:50,
@@ -116,20 +102,5 @@ const styles = StyleSheet.create({
         borderWidth:1,
         borderRadius:15,
         borderColor:"#000000"
-    },
-    submitButton:{
-        width:150,
-        height:40,
-        margin:10,
-        alignItems:"center",
-        justifyContent:"center",
-        alignSelf:"center",
-        backgroundColor:"#32CBD9",
-        borderRadius:10
-    },
-    buttonText:{
-        color:'#000000',
-        fontSize:18,
-        fontWeight:'700'
     }
 })
--- src/screens/ComponentsScreens/Test/TestScreen.js
+++ src/screens/ComponentsScreens/Test/TestScreen.js
@@ -1,17 +1,53 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
-import { Form } from "../../../components/Form/Forms";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { CustomInput } from "../../../components/Input/CustomInput";
 
 export const TestScreen=()=>{
     return(
-        <Form/>
+        <View style={styles.container}>
+            <View style={styles.formContainer}>
+                <CustomInput label="First Name"/>
+                <CustomInput label="Last Name"/>
+                <CustomInput label="City"/>
+                <CustomInput label="Country"/>
+                <CustomInput label="Mobile"/>
+            </View>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity 
+                    style={styles.submitButton}
+                    // onPress={handleInput}
+                >
+                    <Text style={styles.buttonText}>Submit</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
     container:{
         flex:1,
-        justifyContent:"center",
-        alignItems:"center",
+        //justifyContent:'center'
     },
+    formContainer:{
+        flex:1
+    },
+    buttonContainer:{
+        flex:0.25
+    },
+    submitButton:{
+        width:150,
+        height:40,
+        margin:10,
+        alignItems:"center",
+        justifyContent:"center",
+        alignSelf:"center",
+        backgroundColor:"#32CBD9",
+        borderRadius:10
+    },
+    buttonText:{
+        color:'#000000',
+        fontSize:18,
+        fontWeight:'700'
+    }
 });
